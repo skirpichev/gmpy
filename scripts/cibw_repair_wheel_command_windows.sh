ab13063b8b7979724fdd29cced07dae64c0306ff
Sergey B Kirpichev <skirpichev@gmail.com>
2024-03-29 09:03:16 +0300

    XXX

--- scripts/cibw_repair_wheel_command_windows.sh
+++ scripts/cibw_repair_wheel_command_windows.sh
@@ -5,9 +5,8 @@ set -e -x
 wheel=$WHEELNAME
 dest_dir=$WHEELHOUSE
 
-# XXX: this dumps *.dll and *.lib to the site-packages/
-pip install -U git+https://github.com/skirpichev/delvewheel.git@fix-lib-sdir
+pip install -U --force-reinstall git+https://github.com/skirpichev/delvewheel.git@fix-lib-sdir
 delvewheel repair ${wheel} -w ${dest_dir} --add-path .local/bin --no-mangle-all -v
 
 cp .local/bin/{gmp,mpfr,mpc}.lib ${dest_dir}
-(cd ${dest_dir}; wheel unpack --dest . gmpy2-*.whl; mv *.lib gmpy2-*/*/platlib/; wheel pack gmpy2-*[0-9]; rm -rf gmpy2-*[0-9])
+(cd ${dest_dir}; wheel unpack --dest . gmpy2-*.whl; mv *.lib gmpy2-*/gmpy2.libs; wheel pack gmpy2-*[0-9]; rm -rf gmpy2-*[0-9])
