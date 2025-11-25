48f7f2062b2c523b7c87a8808b30a252b7d319e9
Sergey B Kirpichev <skirpichev@gmail.com>
2025-11-25 12:15:24 +0300

    Apply scripts/gcc15.diff

--- scripts/cibw_before_all.sh
+++ scripts/cibw_before_all.sh
@@ -15,6 +15,7 @@ cd gmp-${GMP_VERSION}
 # Patch the mp_bitcnt_t to "unsigned long long int" on WINDOWS AMD64:
 patch -N -Z -p0 < ../scripts/mp_bitcnt_t.diff
 patch -N -Z -p0 < ../scripts/fat_build_fix.diff
+patch -N -Z -p1 < ../scripts/gcc15.diff
 # config.guess uses microarchitecture and configfsf.guess doesn't
 # We replace config.guess with configfsf.guess to avoid microarchitecture
 # specific code in common code.
